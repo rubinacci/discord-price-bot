868467a0ff2bdf9c598c006d75d98b807c1e3b4b
0xSkly <skly.beets@protonmail.com>
2021-10-19 14:25:37 +0200

    initial commit

---
+++ src/bot.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const cron = require("node-cron");
+const { fetchTokenPrice } = require("./price");
+const bot = new Discord.Client();
+require("dotenv").config();
+
+bot.once("ready", async () => {
+  console.log('Bot is online')
+  updateStatus();
+  cron.schedule("*/20 * * * * *", () => {
+    updateStatus();
+  });
+});
+
+const updateStatus = async () => {
+  try {
+    let price = (await fetchTokenPrice()).toFixed(process.env.PRICE_PRECISION);
+    await bot.user.setActivity(`BEETS $${price}`, {type: 'WATCHING'});
+} catch (err) {
+    console.log(`Error updating price status: ${err.message}`);
+  }
+};
+
+const start = async () => {
+  bot.login(process.env.DISCORD_TOKEN);
+};
+
+start();
